59edff547a2d8452dc08353d70d591882ee4cc0b
Sunni Walker <lilsune@Sunnis-MacBook-Air.local>
2022-10-23 00:22:05 -0700

    define a function that accepts a string as a parameter

--- Functions-Problem-1/script.js
+++ Functions-Problem-1/script.js
@@ -1,23 +1,10 @@
 /*
 step 1: define a javascript that accepts a string as a parameter.
-step 2: split the string passed in, turn it into an array of each individual letter,
+step 2: split the string passed in, turn it into an array of each word,
 store it in a variable
-step 3: define a variable called numberOfVowels, and set the inital value to be equal to 0;
-step 4: define a variable that defines what a vowel is, an array of vowels
-step 5: start looping through an array of letters
-step 6: check if the current letter in our loop is a vowel
-step 7: increase the numberOfVowels by one for every letter that's a vowel
-step 8: return the numberOfVowels
+step 3: define a variable that defines what a vowel is, an array of vowels
+step 4: start looping through an array of words
+step 5: return the longest word
 */
 
-var vowelString = 'now you see it';
-
-const stringArray = vowelString.split("");
-
-var numberOfVowels = 0;
-
-const vowelArray = [a, e, i, o, u];
-
-for (let numberOfVowels = 0; numberOfVowels < vowelArray.length; numberOfVowels++) {
-	
-}
+var matrixString = 'you been living in a dream Neo';
