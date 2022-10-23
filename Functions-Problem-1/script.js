8d1116c59d6cb45a9a3eb58867e6e73fa9c01e5a
Sunni Walker <lilsune@Sunnis-MacBook-Air.local>
2022-10-23 00:23:10 -0700

    split the string

--- Functions-Problem-1/script.js
+++ Functions-Problem-1/script.js
@@ -1,10 +1,11 @@
 /*
-step 1: define a javascript that accepts a string as a parameter.
+step 1: define a javascript function that accepts a string as a parameter.
 step 2: split the string passed in, turn it into an array of each word,
 store it in a variable
-step 3: define a variable that defines what a vowel is, an array of vowels
 step 4: start looping through an array of words
 step 5: return the longest word
 */
 
 var matrixString = 'you been living in a dream Neo';
+
+var matrixArray = matrixString.split(" ");
