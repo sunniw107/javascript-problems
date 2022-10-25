2c60d485a81a5525840cbcd36a5df49b60612abf
Sunni Walker <lilsune@Sunnis-MacBook-Air.local>
2022-10-24 19:24:48 -0700

    Step 2

--- Functions-Problem-2/script.js
+++ Functions-Problem-2/script.js
@@ -1,1 +1,11 @@
-script.js
+//step 1: create a string with
+const words = "you been living in a dream Neo"
+
+//step 2: split the string by letters into an array and store it in a variable
+function findthevowels(words) {
+	const letterArray = words.split("")
+//step 3: 
+	let findthevowels = ("")
+}
+
+//step 3:
