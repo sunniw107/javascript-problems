14e11ec04f78c8631f3ac186817cbb172fdf6b99
Sunni Walker <lilsune@Sunnis-MacBook-Air.local>
2022-10-24 18:53:20 -0700

    Finished Function Problem 1

--- Functions-Problem-1/script.js
+++ Functions-Problem-1/script.js
@@ -1,16 +1,23 @@
-/*
-step 1: define a javascript function that accepts a string as a parameter.
-step 2: split the string passed in, turn it into an array of each word,
+
+//step 1: define a javascript function that accepts a string as a parameter.
+let wordString = 'you been living in a dream Neo';
+
+//step 2: split the string passed in, turn it into an array of each word,
 store it in a variable
-step 4: start looping through an array of words
-step 5: return the longest word
-*/
-
-var matrixString = 'you been living in a dream Neo';
-
-var matrixArray = matrixString.split(" ");
-
-for (let i = 0; i < matrixArray.length; 
-	matrixArray.length = matrixArray.length + 1) {
+function findLongestWord(word) {
+	const wordArray = wordString.split(" ");
 	
+	let longestWord = "";
+
+//step 4: start looping through an array of words
+	for (let i = 0; i < wordArray.length; i++) {
+		console.log(wordArray[i].length)
+
+			if (wordArray[i].length > longestWord.length) {
+			longestWord = wordArray[i];
+		}
+	}
+//step 5: return the longest word
+	return longestWord;
 }
+
