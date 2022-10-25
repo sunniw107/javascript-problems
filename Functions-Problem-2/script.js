c5067687f2e04e6595fd3cd337e0a8ba86602927
Sunni Walker <lilsune@Sunnis-MacBook-Air.local>
2022-10-24 20:36:06 -0700

    Step 5

--- Functions-Problem-2/script.js
+++ Functions-Problem-2/script.js
@@ -1,11 +1,24 @@
 //step 1: create a string with
-const words = "you been living in a dream Neo"
+const wordString = "you been living in a dream Neo";
 
 //step 2: split the string by letters into an array and store it in a variable
-function findthevowels(words) {
-	const letterArray = words.split("")
-//step 3: 
-	let findthevowels = ("")
+function findthevowels(word) {
+	const letterArray = word.split("");
+
+//step 3: define a variable and set the value to 0
+	let vowelCount = "";
+
+//step 4: define a variable that knows what a vowel is using a vowel array
+	vowelArray = [a, e, i, o, u]
+
+//step 5: start looping through an array of letters
+	for (let i = 0; i < letterArray.length; i++) {
+
+//step 6: check the letters in our loop for a vowel
+	
+
+//step 7: 
+	}
 }
 
-//step 3:
+
