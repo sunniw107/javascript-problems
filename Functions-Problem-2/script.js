d8bd79293b0669a4ea176ed83e32d20a6fc11548
Sunni Walker <lilsune@Sunnis-MacBook-Air.local>
2022-10-26 22:08:26 -0700

    Funct Prob 2: forEach loop

--- Functions-Problem-2/script.js
+++ Functions-Problem-2/script.js
@@ -2,23 +2,29 @@
 const wordString = "you been living in a dream Neo";
 
 //step 2: split the string by letters into an array and store it in a variable
-function findthevowels(word) {
-	const letterArray = word.split("");
+function findthevowels(wordString) {
+	const letterArray = wordString.split("");
 
 //step 3: define a variable and set the value to 0
 	let vowelCount = "";
 
 //step 4: define a variable that knows what a vowel is using a vowel array
-	vowelArray = [a, e, i, o, u]
+	const vowelArray = [a, e, i, o, u];
 
 //step 5: start looping through an array of letters
 	for (let i = 0; i < letterArray.length; i++) {
 
 //step 6: check the letters in our loop for a vowel
-	
+	vowelArray.forEach(item => {
+		if(vowelCount[item]) {
+			vowelCount[item]++;
+		} else {
+			vowelCount[item] = 1;
+		}
+	});
 
 //step 7: 
 	}
 }
 
-
+console.log(vowelCount)
