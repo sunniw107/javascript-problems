f6529df2cd14d4b4a15455f40e54c6415e9dcc44
Sunni Walker <lilsune@Sunnis-MacBook-Air.local>
2022-10-26 21:33:37 -0700

    Corrected: Passed In String

--- Functions-Problem-1/script.js
+++ Functions-Problem-1/script.js
@@ -1,10 +1,9 @@
-
 //step 1: define a javascript function that accepts a string as a parameter.
 let wordString = 'you been living in a dream Neo';
 
 //step 2: split the string passed in, turn it into an array of each word,
-store it in a variable
-function findLongestWord(word) {
+//store it in a variable
+function findLongestWord(wordString) {
 	const wordArray = wordString.split(" ");
 	
 	let longestWord = "";
@@ -13,10 +12,11 @@ function findLongestWord(word) {
 	for (let i = 0; i < wordArray.length; i++) {
 		console.log(wordArray[i].length)
 
-			if (wordArray[i].length > longestWord.length) {
+			if (wordArray[i].length > wordString.length) {
 			longestWord = wordArray[i];
 		}
 	}
+
 //step 4: return the longest word
 	return longestWord;
 }
