4c40c452e03d68f0a1f19f299fa03c9c8bee62c1
Sunni Walker <lilsune@Sunnis-MacBook-Air.local>
2022-11-02 01:09:12 -0700

    Function Problem 3 check?

--- Functions-Problem-3/script.js
+++ Functions-Problem-3/script.js
@@ -1,15 +1,16 @@
 //step 1: define a function that accepts two arguments a string and a letter
-function letterCount(){
-	const letterStr = "you been living in a dream Neo";
-	const eArray = ["e"]
+function letterCount(stringForLetters, letter){
+	let letterCount = 0
 //step 2: split the string into an array of letters
-	let letterArray = letterStr.split("");
+	let letterArray = stringForLetters.split("");
 
 //step 3: define a loop that will run through the letters of the string
-	for (let i = 0; i < letterArray.length; i++){
+	letterArray.forEach(function(letterInArray)){
 //step 4: count the number of occurences of the specified letter via the loop
-	if (eArray.includes(letterArray[i]))
-	}
+	if (letterInArray.toLowerCase() === letter.toLowerCase()) {
+		letterCount += 1;
+		}
+	};
 //step 5: return the count of the specified letter
-
+	return letterCount;
 }
