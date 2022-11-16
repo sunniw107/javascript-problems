12879a60dc624d83588d519c41c6077a282f2b5a
Sunni Walker <lilsune@Sunnis-MacBook-Air.local>
2022-11-16 14:29:06 -0800

    Problem Complete?

--- Functions-Problem-3/script.js
+++ Functions-Problem-3/script.js
@@ -5,12 +5,12 @@ function letterCount(stringForLetters, letter){
 	let letterArray = stringForLetters.split("");
 
 //step 3: define a loop that will run through the letters of the string
-	letterArray.forEach(function(letterInArray)){
+	letterArray.forEach(function(letterInArray){
 //step 4: count the number of occurences of the specified letter via the loop
 	if (letterInArray.toLowerCase() === letter.toLowerCase()) {
 		letterCount += 1;
 		}
-	};
+	});
 //step 5: return the count of the specified letter
 	return letterCount;
 }
